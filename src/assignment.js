a371a575e3ed42d76bbd6743b01660873fe479fb
Salami Faruk <54101782+salami-faruk@users.noreply.github.com>
2022-07-15 15:00:17 +0000

    Check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -6,9 +6,7 @@ const assignment = {};
 
 /**
  * Challenge - 1
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+ 
 function sumOfNumbers(arrayOfNumbers) {
   let sum = 0;
   for(let a of arrayOfNumbers)
@@ -20,13 +18,7 @@ console.log(sumOfNumbers([3,4,2,1]))
 
 /**
  * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+ 
 function countEvenNumbers(arrayOfNumbers) {
    let count = 0;
    for(let a of arrayOfNumbers){
