ff7a2386ff9145aabbaf54aecc4ca4a937e25cce
Salami Faruk <54101782+salami-faruk@users.noreply.github.com>
2022-07-15 14:41:48 +0000

    Check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -6,17 +6,17 @@ const assignment = {};
 
 /**
  * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for(let a of arrayOfNumbers)
+  sum +=a;
+  return sum;
+  return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+console.log(sumOfNumbers([3,4,2,1]))
 
 /**
  * Challenge - 2
@@ -28,29 +28,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let count = 0;
+   for(let a of arrayOfNumbers){
+   if(a % 2 === 0){
+  count ++;
+        }
+    }
+    return count;
+    
 }
+console.log(countEvenNumbers([3,4,2,1]))
 // assignment.countEvenNumbers = countEvenNumbers;
 
-/**
- * Challenge - 3
- * 
- * Given an array of numbers representing temperatures in Celsius,
- * convert each number to Fahrenheit and return a new array containing the converted
- * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
- * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
- * If you did Challenge - 3, remove the comment in the line just after this function
- * 
- * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
- * 
- * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
- * @returns Array the converted temperatures in Fahrenheit
- */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
+ 
+ 
 
 // ========================
 // DO NOT EDIT THIS BLOCK
